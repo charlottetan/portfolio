8cedac3d86482653fb99c3b4f26deff5bf6aeb66
charlottetan <charlottetan@users.noreply.github.com>
2016-02-16 22:52:28 -0800

    better scrolling fn

--- client/app.js
+++ client/app.js
@@ -33,24 +33,34 @@ if (Meteor.isClient) {
   });
 
   Template.navItems.rendered = function () {
-    // Cache selectors
+    // Selectors
     let lastId = null;
     let topMenu = $("nav .container .menu");
     // All list items
     let menuItems = topMenu.find("a");
     // Anchors corresponding to menu items
-    let scrollItems = menuItems.map(function(){
-      // look for the element with the id in the menu's href e.g. #portfolio
-      let item = $($(this).attr("href") + "-section");
-      if (item.length) { return item; }
-    });
+    let sections = $("main section");
+    // we will consider the section displayed if the elementTop
+    // shows up between these values
+    let navMenuHeight = 45;
+    let viewportTopTolerance = navMenuHeight;
+    let viewportBottomTolerance = $(window).height() * 0.70;
 
+    // Because this is used for the jQuery collection filter,
+    // not the JS array filter
+    let isInView = function(index, element) {
+      let elementTop = $(element).offset().top - $(window).scrollTop();
+
+      if (elementTop >= viewportTopTolerance && elementTop <= viewportBottomTolerance) {
+        return true;
+      }
+    };
 
     // Bind click handler to menu items
     // so we can get a fancy scroll animation
     menuItems.click(function(e){
       let href = $(this).attr("href");
-      let offsetTop = href === "#home" ? 0 : $(href + "-section").offset().top-45;
+      let offsetTop = href === "#home" ? 0 : $(href + "-section").offset().top-navMenuHeight;
       $('html, body').stop().animate({ 
           scrollTop: offsetTop
       }, 300);
@@ -58,41 +68,23 @@ if (Meteor.isClient) {
     });
 
     // Bind to scroll
-    $(window).scroll(function(){
-       // Get container scroll position
-       let fromTop = $(this).scrollTop();
-
-       let windowHeight = $(this).height();
-       let docHeight = $(document).height();
-       let bottomTolerance = 100;
-       
+    $(window).scroll(function() {
        // Get id of current scroll item
-       let candidates = scrollItems.map(function(){
-         if ($(this).offset().top < fromTop+46) {
-           return this;
+       let sectionId = sections.filter(isInView).attr("id");
+
+       // if none, means no section top is in viewport
+       // so don't change the id
+       if (sectionId) {
+        let navId = sectionId.slice(0, sectionId.indexOf('-'));
+
+         if (lastId !== navId) {
+             lastId = navId;
+             // Set/remove active class
+             menuItems
+               .removeClass("active")
+               .filter("[href=#"+navId+"]").addClass("active");
          }
-       });
-
-       // Get the last item in the list
-       let cur = candidates[candidates.length-1];
-
-       // check if we're near the bottom
-       if (fromTop + windowHeight > docHeight - bottomTolerance) {
-        // then actually it's the last item, because it never reaches the nav bar's height
-        cur = $(scrollItems[scrollItems.length-1]);
        }
-
-       // Get the id of the current element
-       let id = cur && cur.length ? cur[0].id : "home-section";
-       id = id.slice(0, id.indexOf('-'));
-
-       if (lastId !== id) {
-           lastId = id;
-           // Set/remove active class
-           menuItems
-             .removeClass("active")
-             .filter("[href=#"+id+"]").addClass("active");
-       }                   
-    });
+    }); // end bind to scroll
   };
 }
