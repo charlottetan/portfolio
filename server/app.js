33a320533ed3977c4874facd3a6164edb3dd4c43
charlottetan <charlottetan@users.noreply.github.com>
2016-02-24 19:12:06 -0800

    remove okgrow:analytics because it uses http

--- server/app.js
+++ server/app.js
@@ -1,28 +1,33 @@
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    
-    if (Projects.find().count() === 0) {
+    let connectHandlers = WebApp.connectHandlers;
+    connectHandlers.use(function(req, res, next) {
+        console.log("detected");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        return next();
+    });
 
-        // these would be better in a json file
-        // but I wanted to play around with mongo
+    Meteor.startup(function () {
 
-        // possible fields:
-        // title, url, src, img, tech, desc
-        var bootstrapData = [
-          {
-            title: 'Portfolio', 
-            url  : 'http://charlottetan.herokuapp.com',
-            src  : 'https://github.com/charlottetan/portfolio',
-            tech : ['Meteor', 'Semantic UI', 'Heroku'],
-            desc : 'This site! Simple single page app to experiment with new frameworks.'
-          }
-        ];
+        if (Projects.find().count() === 0) {
 
-        _.each(bootstrapData, function(doc) { 
-            Projects.insert(doc);
-        })
-    }
-  });
+            // these would be better in a json file
+            // but I wanted to play around with mongo
+
+            // possible fields:
+            // title, url, src, img, tech, desc
+            let bootstrapData = [{
+                title: 'Portfolio', 
+                url  : 'http://charlottetan.herokuapp.com',
+                src  : 'https://github.com/charlottetan/portfolio',
+                tech : ['Meteor', 'Semantic UI', 'Heroku'],
+                desc : 'This site! Simple single page app to experiment with new frameworks.'
+            }];
+
+            _.each(bootstrapData, function(doc) { 
+                Projects.insert(doc);
+            });
+        }
+    });
 
     Meteor.publish("projects", function() {
         return Projects.find();
